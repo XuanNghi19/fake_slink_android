dc0b7387c22bb2cbb0096c700fa872fcab06c42b
XuanNghi19 <XuanNghi19t@outlook.com>
2024-10-29 18:31:25 +0700

    All UI DONE

---
+++ app/src/main/java/com/example/fake_slink/activities/NotificationDetailActivity.kt
@@ -0,0 +1,21 @@
+package com.example.fake_slink.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.fake_slink.R
+
+class NotificationDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_notification_detail)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
